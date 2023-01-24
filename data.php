5417774ce68ed1145d1a0b6f2a0b9aa04ff84ec7
Shelbot77 <alessiodeangelis021@gmail.com>
2023-01-24 16:19:01 +0100

    Stampo a schermo lunghezza paragrafo

--- data.php
+++ data.php
@@ -1,7 +1,15 @@
 <h2>
     <?php
     echo $_POST['paragrafo'];
+    //strlen(stringa) - ritorna la lunghezza di una stringa
+
 
     ?>
 
 </h2>
+<h3>
+    <?php
+    echo strlen($_POST['paragrafo']);
+    ?>
+
+</h3>
